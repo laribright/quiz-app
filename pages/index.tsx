be288bb49a56768c1c82ed476b3549e6ad7c6e4b
laribright <laribright8@gmail.com>
2022-11-13 23:11:15 +0100

    clean up more boiler plate code from next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import Head from "next/head";
 
-import styles from "@/pages/index.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="text-green">Hello world</main>
+      <main className="text-green-900">Hello world</main>
     </div>
   );
 }
