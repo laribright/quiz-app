4f13535f9ddd2be36c7176f352f5f133d3853559
laribright <laribright8@gmail.com>
2022-11-14 02:20:26 +0100

    Pass props to Notification Modal

--- __tests__/components/NotificationModal.test.tsx
+++ __tests__/components/NotificationModal.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import NotificationModal from "@/components/NotificationModal";
 
 describe("<NotificationModal />", () => {
-  const setup = () => render(<NotificationModal />);
+  const setup = () => render(<NotificationModal isCorrect={true} />);
 
   it("renders the notification component", () => {
     setup();
