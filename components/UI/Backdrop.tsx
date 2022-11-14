fcf64ae807caebbeb7459f93aaf2bd4345e2a6d3
laribright <laribright8@gmail.com>
2022-11-14 02:21:09 +0100

    Style backdrop component

--- components/UI/Backdrop.tsx
+++ components/UI/Backdrop.tsx
@@ -1,5 +1,9 @@
 const Backdrop = () => {
-  return <div data-testid="backdrop">Backdrop</div>;
+  return (
+    <div data-testid="backdrop" className="fixed z-10 bg-black w-full h-full bg-opacity-80">
+      Backdrop
+    </div>
+  );
 };
 
 export default Backdrop;
