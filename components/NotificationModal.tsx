33f8daf508c9f4ad6314d03c6f67d61531cfdc53
laribright <laribright8@gmail.com>
2022-11-14 03:21:34 +0100

    Conditionally render success or failure text... Hide notification modal when play again button is clicked

--- components/NotificationModal.tsx
+++ components/NotificationModal.tsx
@@ -5,23 +5,35 @@ import ThumbsUp from "../public/assets/images/thumbsUp.png";
 
 interface NotificationModalProps {
   isCorrect: boolean;
+  playAgainFn: () => void;
 }
 
 const NotificationModal: FC<NotificationModalProps> = (props) => {
-  const { isCorrect } = props;
+  const { isCorrect, playAgainFn } = props;
 
   return (
     <div
       data-testid="notification-modal"
       className="w-[50vw] p-3 fixed z-20 text-center bg-white rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
     >
-      <h4 className="mb-5 font-semibold text-green-700">
-        Congrats You Passed the Quiz
-      </h4>
+      {isCorrect ? (
+        <h4 className="mb-5 md:text-4xl font-semibold text-green-700">
+          Congrats You Passed the Quiz
+        </h4>
+      ) : (
+        <h4 className="mb-5 md:text-4xl font-semibold text-green-700">
+          Opps, not correct this time
+        </h4>
+      )}
 
-      <Image src={ThumbsUp} alt="Thumbs Up" />
+      {isCorrect && <Image src={ThumbsUp} alt="Thumbs Up" />}
 
-      <button className="mt-5 bg-pink-500 text-white px-4 py-2 rounded-xl">Play Again</button>
+      <button
+        onClick={playAgainFn}
+        className="mt-5 bg-pink-500 text-white px-4 py-2 rounded-xl"
+      >
+        Play Again
+      </button>
     </div>
   );
 };
