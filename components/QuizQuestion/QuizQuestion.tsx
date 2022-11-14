980c2ed2e6262dabec8824ccb6bb383faf794ef8
laribright <laribright8@gmail.com>
2022-11-14 03:21:48 +0100

    Add question label text

--- components/QuizQuestion/QuizQuestion.tsx
+++ components/QuizQuestion/QuizQuestion.tsx
@@ -8,6 +8,7 @@ const QuizQuestion: FC<{ question: string }> = (props) => {
       data-testid="question-component"
       className="text-center border-pink-400 border-[2px] text-2xl font-semibold py-3 rounded-sm"
     >
+      <h5 className="text-lg text-gray-600">Question</h5>
       <h2>{question}</h2>
     </div>
   );
