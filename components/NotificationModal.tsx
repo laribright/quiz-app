3c70cf7501eac0cfc3c946b59acfdb3ddd76f3b5
laribright <laribright8@gmail.com>
2022-11-14 01:52:11 +0100

    Scaffold the notification component

---
+++ components/NotificationModal.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const NotificationModal: FC = (props) => {
+  return <div data-testid="notification-modal">NotificationModal</div>;
+};
+
+export default NotificationModal;
